1a52e4badb2d3665539619a81575046525c4a78c
safiullah28 <safiullah424@gmail.com>
2024-03-13 00:13:11 +0500

    Context API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{ createContext } from "react";
+import ChildA from "./childA";
+
+let data1 = createContext();
+let data2=createContext();
 
 function App() {
+  const name = "Safiullah";
+  const age=21;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <data1.Provider value={name}>
+      <data2.Provider value={age}>
+
+        <ChildA />
+
+        </data2.Provider>
+      </data1.Provider>
     </div>
   );
 }
 
 export default App;
+export { data1,data2 };
+
+//Create
+//Provider
+// User
